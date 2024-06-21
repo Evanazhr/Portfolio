94b2b7d35c478a04ddbfac3111531d35192be964
Evanazhr <evanazhr@gmail.com>
2024-06-21 10:34:06 +0700

    change import speed insight

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import Footer from "./components/Footer"
 import RoutesIndex from "./routes/Routes.jsx"
 
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
     return (
         <main className=" flex w-full flex-row bg-dark-background font-inter lg:m-auto lg:max-w-5xl">
                 <Analytics/>
                 <SpeedInsights/>
+                
                 <Navbar />
                 <div className="flex w-full flex-col">
                     <RoutesIndex />
