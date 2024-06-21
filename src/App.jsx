777c93d1d0e8847e68b7ec39819b2485071416cc
Evanazhr <evanazhr@gmail.com>
2024-06-21 09:59:57 +0700

    add analytic and insight vercel

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
 import RoutesIndex from "./routes/Routes.jsx"
 
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 function App() {
     return (
-        <main className="scroll-smooth flex w-full flex-row bg-dark-background font-inter lg:m-auto lg:max-w-5xl">
+        <main className=" flex w-full flex-row bg-dark-background font-inter lg:m-auto lg:max-w-5xl">
                 <Navbar />
                 <div className="flex w-full flex-col">
                     <RoutesIndex />
